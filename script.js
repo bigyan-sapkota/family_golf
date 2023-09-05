ff6004cabd353a0d08895fb2ae1a8531907caf48
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-09-06 00:11:40 +0545

    completed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const blur = document.querySelector("#cursor-blur");
 const h4all = document.querySelectorAll("#nav h4");
 
 document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + 40 + "px";
+  crsr.style.left = dets.x + "px";
   crsr.style.top = dets.y + "px";
   blur.style.left = dets.x - 250 + "px";
   blur.style.top = dets.y - 250 + "px";
@@ -55,8 +55,56 @@ gsap.from("#about-us img, #about-us-in", {
   scrollTrigger: {
     trigger: "#about-us",
     scroller: "body",
-    start: "top 60%",
-    end: "top 58%",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+
+gsap.from(".card", {
+  scale: 0.8,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 2,
+  },
+});
+
+gspa.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    start: "top 75%",
+    end: "top 70%",
     scrub: 3,
   },
 });
